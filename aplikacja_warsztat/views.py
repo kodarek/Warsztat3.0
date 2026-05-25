0754b6a62645e1fe26ecc943b580c166c3d51a2e
Dariusz Kozaczuk <dkozaczuk36@gmail.com>
2026-05-25 20:30:06 +0200

    dodanie wpisu awarii

--- aplikacja_warsztat/views.py
+++ aplikacja_warsztat/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Awarie
 
 # Create your views here.
 def awarie(request):
+    awaria = Awarie.objects.all()
     return render(request, 'aplikacja_warsztat/awarie.html', {})
